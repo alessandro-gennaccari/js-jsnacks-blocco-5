b2470af42efe3aa32345e6546de9978fc76d0163
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-01-18 14:43:35 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/* Jsnack1 - Creare un oggetto palla che abbia le seguenti proprietà.
+Nome = palla
+Peso = 10
+Attraverso un prompt dare la possibilità all'utente di modificare il peso della palla. */
+
+
+// ---------------------------------------------------------------------------------------
+
+
+
+/* Jsnack2 - Creare un array che contiene 10 oggetti che rappresentano una zucchina,
+indicandone per ognuno varietà, peso e lunghezza.
+Calcola quanto pesano tutte le zucchine. */
+
+
+
+// ---------------------------------------------------------------------------------------
+
+
+
+/* Jsnack3 - Creare  un array che contiene 10 oggetti che rappresentano una zucchina.
+Dividi in due array separati le zucchine che misurano meno o più di 15cm.
+Infine stampa separatamente quanto pesano i due gruppi di zucchine. */
+
+
+
+// ---------------------------------------------------------------------------------------
